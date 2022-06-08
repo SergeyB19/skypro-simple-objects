a44b016c41809e691b9a6758e48ca620b66e53bf
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-06-08 12:46:57 +0300

    Лекция

---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    String name;
+    int age;
+
+    public static Person Person(String name, int age) {
+        Person person = new Person();
+        person.name = name;
+        person.age = age;
+        return person;
+
+    }
+}
